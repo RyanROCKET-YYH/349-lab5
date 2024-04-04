27aefd846b9712e0ea52ee3deb966be0c99604e2
Yuhong Yao <yuhongy@andrew.cmu.edu>
2024-04-04 07:09:48 -0400

    tested section 4

--- src/adc.c
+++ src/adc.c
@@ -58,7 +58,7 @@ struct adc_reg_map {
 #define ADC1_CR2_CONT  (1 << 1)
 
 /** @brief SQR2 SQ11[4:0] */
-#define ADC1_SQR2_SQ11 (31 << 20)
+#define ADC1_SQR1_SQ1 (31 << 0)
 
 /** @brief Channel Squence length -- 11 in this case */
 #define SEQUENCE_LENGTH (10 << 20)
@@ -74,29 +74,27 @@ void adc_init(){
 	struct rcc_reg_map *rcc = RCC_BASE;
 	rcc->apb2_enr |= ADC_CLKEN;
 
-	// GPIO Pins(PC_1, A4)(ADC1/11) 
-	gpio_init(GPIO_C, 1, MODE_ANALOG_INPUT, OUTPUT_PUSH_PULL, OUTPUT_SPEED_LOW, PUPD_NONE, ALT0);
+	// GPIO Pins(PA_0, A0)(ADC1/0) 
+	gpio_init(GPIO_A, 0, MODE_ANALOG_INPUT, OUTPUT_PUSH_PULL, OUTPUT_SPEED_LOW, PUPD_NONE, ALT0);
 
 	// set adc
 	struct adc_reg_map *adc = ADC1_BASE;
+	// set 10 bit resolution for the ADC
+	adc->CR1 &= ~(ADC1_CR1_RES_HI); // 0
+	adc->CR1 |= ADC1_CR1_RES_LO; // 1
 
 	// turn on adc power
 	adc->CR2 |= ADC1_CR2_ADON;
 	// single conversion mode
 	adc->CR2 &= ~(ADC1_CR2_CONT); // enable single conversion mode
-	// adc->CR2 |= ADC1_CR2_SWSTART;
-
-	// set 10 bit resolution for the ADC
-	adc->CR1 &= ~(ADC1_CR1_RES_HI); // 0
-	adc->CR1 |= ADC1_CR1_RES_LO; // 1
 	return;
 }
 
 uint16_t adc_read_chan(uint8_t chan){
 	struct adc_reg_map *adc = ADC1_BASE;
 
-	adc->SQR2 &= ~ADC1_SQR2_SQ11;
-	adc->SQR2 |= (chan << 20);
+	adc->SQR2 &= ~ADC1_SQR1_SQ1;
+	adc->SQR2 |= chan;
 
 	adc->SQR1 &= ~ADC1_SQR1_L;
 	adc->SQR1 |= SEQUENCE_LENGTH;
--- src/main.c
+++ src/main.c
@@ -10,7 +10,36 @@
 
 #include <FreeRTOS.h>
 #include <task.h>
+#include <adc.h>
+#include <stdio.h>
+#include <uart.h>
+
+
+void vADCReadTask(void *pvParameters) {
+    (void)pvParameters;
+    adc_init(); 
+    while(1) {
+        uint16_t adcValue = adc_read_chan(0);
+        printf("ADC Value: %u\n", adcValue);
+
+        vTaskDelay(pdMS_TO_TICKS(500));
+    }
+}
 
 int main( void ) {
+    uart_init(115200);
+
+    xTaskCreate(
+        vADCReadTask,
+        "ADCRead",
+        configMINIMAL_STACK_SIZE,
+        NULL,
+        tskIDLE_PRIORITY + 1,
+        NULL);
+
+    vTaskStartScheduler();
+    
+    // Infinite loop
+    for(;;) {}  
     return 0;
 }
